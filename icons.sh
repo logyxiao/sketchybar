c2484365fb8e7fd6d0aa128e0af0d50abfb9160d
yexiaojian <yexiaojian@binmade.cn>
2024-04-20 03:26:41 +0800

    feat: init

---
+++ icons.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# General Icons
+LOADING=􀖇
+APPLE=􀣺
+PREFERENCES=􀺽
+ACTIVITY=􀒓
+LOCK=􀒳
+BELL=􀋚
+BELL_DOT=􀝗
+WECHAT=󰘑
+QQ=󰘅
+WIFI_ON=􀷗
+WIFI_OFF=􂄂
+
+# Git Icons
+GIT_ISSUE=􀍷
+GIT_DISCUSSION=􀒤
+GIT_PULL_REQUEST=􀙡
+GIT_COMMIT=􀡚
+GIT_INDICATOR=􀂓
+
+# Spotify Icons
+SPOTIFY_BACK=􀊎
+SPOTIFY_PLAY_PAUSE=􀊈
+SPOTIFY_NEXT=􀊐
+SPOTIFY_SHUFFLE=􀊝
+SPOTIFY_REPEAT=􀊞
+
+# Yabai Icons
+YABAI_STACK=􀏭
+YABAI_FULLSCREEN_ZOOM=􀏜
+YABAI_PARENT_ZOOM=􀥃
+YABAI_FLOAT=􀢌
+YABAI_GRID=􀧍
+
+# Battery Icons
+BATTERY_100=􀛨
+BATTERY_75=􀺸
+BATTERY_50=􀺶
+BATTERY_25=􀛩
+BATTERY_0=􀛪
+BATTERY_CHARGING=􀢋
+
+# Volume Icons
+VOLUME_100=􀊩
+VOLUME_66=􀊧
+VOLUME_33=􀊥
+VOLUME_10=􀊡
+VOLUME_0=􀊣
+
+
+# General Icons
+export LOADING=􀖇
+export APPLE=􀣺
+export PREFERENCES=􀺽
+export ACTIVITY=􀒓
+export LOCK=􀒳
+export LOGOUT=
+export POWER=
+export REBOOT=
+export SLEEP=⏾
+export BELL=􀋚
+export BELL_DOT=􀝗
+
+export BATTERY=
+export CPU=
+export DISK=
+export MEMORY=﬙
+export NETWORK=
+export NETWORK_DOWN=
+export NETWORK_UP=
+
+# Git Icons
+export GIT_ISSUE=􀍷
+export GIT_DISCUSSION=􀒤
+export GIT_PULL_REQUEST=􀙡
+export GIT_COMMIT=􀡚
+export GIT_INDICATOR=
+
+# Spotify Icons
+export SPOTIFY_BACK=􀊎
+export SPOTIFY_PLAY_PAUSE=􀊈
+export SPOTIFY_NEXT=􀊐
+export SPOTIFY_SHUFFLE=􀊝
+export SPOTIFY_REPEAT=􀊞
+
+# Yabai Icons
+export YABAI_STACK=􀏭
+export YABAI_FULLSCREEN_ZOOM=􀏜
+export YABAI_PARENT_ZOOM=􀥃
+export YABAI_FLOAT=􀢌
+export YABAI_GRID=􀧍
---
+++ plugins/calendar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sketchybar --set $NAME icon="" label="$(date '+%d/%m %H:%M')"
